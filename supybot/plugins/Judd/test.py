3eb067eee075f91be7dde9b981fb2cdb30f1b463
Stuart Prescott <stuart+debian@nanonanonano.net>
2010-02-21 19:21:52 +0000

    Add test suite to Judd
      * probe a reasonable selection of commands as a test suite that can be used
        either by the supybot-test program or by the livetest program that is part
        of the Piccy plugin

--- supybot/plugins/Judd/test.py
+++ supybot/plugins/Judd/test.py
@@ -33,5 +33,85 @@ from supybot.test import *
 class DebianTestCase(PluginTestCase):
     plugins = ('Judd',)
 
+    def testVersion(self):
+        self.assertNotError('versions libc6')                   # all versions; show oldstable->experimental
+        self.assertNotError('versions libc6 --release sid')     # one version; only show sid
+        self.assertNotError('versions libc6 --arch amd64')      # one arch; show amd64 instead
+        self.assertNotError('versions libc6 --release qwerty')  # no such release; should default to lenny
+        self.assertNotError('versions libc6 --arch qwerty')     # no such arch; should default to i386
+        self.assertNotError('versions nosuchpackage')           # package not found; no such package in the archive
+
+    def testInfo(self):
+        self.assertNotError('info libc6')                       # package info stable; only show stable
+        self.assertNotError('info libc6 --release sid')         # package info sid; only show sid
+        self.assertNotError('info nosuchpackage')               # package not found; no such package in the archive
+
+    def testArch(self):
+        self.assertNotError('arches libc6')                     # arch-dep package; lots of arches for libc6
+        self.assertNotError('arches python')                    # arch:all package; all
+        self.assertNotError('arches nosuchpackage')             # package not found; no such package in the archive
+
+    def testDepends(self):
+        self.assertNotError('depends texlive')                  # depends; some packages
+        self.assertNotError('depends dpkg')                     # depends; no packages
+        self.assertNotError('depends nosuchpackage')            # package not found; no such package in the archive
+
+    def testRecommends(self):
+        self.assertNotError('recommends python-pyx')            # recommends; a package
+        self.assertNotError('recommends texlive')               # recommends; no packages
+        self.assertNotError('recommends nosuchpackage')         # package not found; no such package in the archive
+
+    def testSuggests(self):
+        self.assertNotError('suggests texlive')                 # suggests; a package
+        self.assertNotError('suggests locales')                 # suggests; no packages
+        self.assertNotError('suggests nosuchpackage')           # package not found; no such package in the archive
+
+    def testConflicts(self):
+        self.assertNotError('conflicts python')                  # some conflicts; several packages
+        self.assertNotError('conflicts texlive')                 # no conflicts; no packages conflict
+        self.assertNotError('conflicts nosuchpackage')           # package not found; no such package in the archive
+
+    def testProvides(self):
+        self.assertNotError('provides postfix')                 # provided packages; m-t-a
+        self.assertNotError('provides nosuchpackage')           # package not found; no such package in the archive
+
+    def testRprovides(self):
+        self.assertNotError('rprovides mail-transport-agent')   # find rproviders; several packages provide m-t-a
+        self.assertNotError('rprovides nosuchpackage')          # no rproviders; no packages provide this
+
+    def testSource(self):
+        self.assertNotError('src libc6 --release lenny')        # source package; glibc has libc6 in lenny
+        self.assertNotError('src libc6 --release sid')          # source package; eglibc has libc6 in sid
+        self.assertNotError('src nosuchpackage')                # package not found; no such package in the archive
+
+    def testBinaries(self):
+        self.assertNotError('binaries python-defaults')         # list binary packages; several binary packages
+        self.assertNotError('binaries pyxplot')                 # list binary packages; only one binary package
+        self.assertNotError('binaries nosuchpackage')           # package not found; no such package in the archive
+
+    def testBuilddep(self):
+        self.assertNotError('builddep perl')                    # simple build-deps; B-D only
+        self.assertNotError('builddep texlive')                 # complex build-deps; B-D and B-D-I
+        self.assertNotError('builddep libc6')                   # build-deps by binary; give build-deps of (e)glibc package
+        self.assertNotError('builddep nosuchpackage')           # package not found; no such package in the archive
+
+    def testPopcon(self):
+        self.assertNotError('popcon perl')                      # list popcon; popular package
+        self.assertNotError('popcon nosuchpackage')             # package not found; no such package in the archive
+
+    def testMaintainer(self):
+        self.assertNotError('maintainer perl')                  # show maintainer; maintianer, uploader and changer
+        self.assertNotError('maintainer nosuchpackage')         # package not found; no such package in the archive
+
+    def testBugs(self):
+        # this is known to be broken at present
+        pass
+
+    def testFile(self):
+        self.assertNotError('file /usr/bin/perl')               # absolute file exists ; /usr/bin/perl in perl
+        self.assertNotError('file bin/perl')                    # fragment file exists ; /usr/bin/perl in perl
+        self.assertNotError('file *bin/perl*')                  # fragment file exists ; /usr/bin/perl in perl, /usr/bin/perldoc in perldoc
+        self.assertNotError('file nvidia-glx')                  # fragment file exists in non-free; various nvidia package should match this
+        self.assertNotError('file /nosuchpackage')              # fragment doesn't exist ; no package contains
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
